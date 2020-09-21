df4239159d945c3a148c51a9c1d3cc4bccc69a33
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-09-21 11:36:49 -0300

    Add doc.

--- closure/main.go
+++ closure/main.go
@@ -2,20 +2,26 @@ package main
 
 import "fmt"
 
+// Component is the component we want to use inside a job
 type Component interface {
 	Do(input interface{})
 }
 
+// component is a Component implementation.
 type component struct{}
 
+// Do does something with the given input. In this case it appends the input to the Hello World string and
+// shows it in the std output.
 func (component) Do(input interface{}) {
 	fmt.Println("Hello world!", input)
 }
 
+// NewComponent initializes a new Component.
 func NewComponent() Component {
 	return &component{}
 }
 
+// main represents an action creation.
 func main() {
 	c := NewComponent()
 	fn := NewFunc(c)
