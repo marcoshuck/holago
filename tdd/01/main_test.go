fcf9141bce5350941b09f6dc783adf958907203a
Marcos Huck <marcos@huck.com.ar>
2021-04-09 10:21:42 -0300

    Add TDD example.

---
+++ tdd/01/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import "testing"
+
+func TestDictionary(t *testing.T) {
+	// Input
+	const (
+		a = "hello"
+	)
+
+	// Output
+	const (
+		b = "a way of greeting people"
+	)
+
+	if GetDefinition(map[string]string{
+		a: b,
+	}, a) != b {
+		t.FailNow()
+	}
+}
+
+func TestDictionaryWhenPassingADifferentWord(t *testing.T) {
+	// Input
+	const (
+		a = "hola"
+	)
+
+	// Output
+	const (
+		b = "una manera de saludar a alguien"
+	)
+
+	if GetDefinition(map[string]string{
+		a: b,
+	}, a) != b {
+		t.FailNow()
+	}
+}
+
+func GetDefinition(data map[string]string, key string) string {
+	if  {
+		
+	}
+	return data[key]
+}
